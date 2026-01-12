c6c587ee76fa37311fa69dd534d96d46e9c7756d
davidyyyyyyyyy <dbattlecardemil@gmail.com>
2026-01-12 18:32:50 -0500

    working relocalization ish

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/limelight/TurretTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/limelight/TurretTele.java
@@ -1,6 +1,9 @@
 package org.firstinspires.ftc.teamcode.drive.limelight;
 
 import static org.firstinspires.ftc.teamcode.robot.Bob.helpers.BobConstants.KALMAN_TURRET;
+import static org.firstinspires.ftc.teamcode.robot.Bob.helpers.BobConstants.LAST_HEADING;
+import static org.firstinspires.ftc.teamcode.robot.Bob.helpers.BobConstants.LAST_X;
+import static org.firstinspires.ftc.teamcode.robot.Bob.helpers.BobConstants.LAST_Y;
 
 import android.annotation.SuppressLint;
 
@@ -15,6 +18,8 @@ import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+//import org.firstinspires.ftc.teamcode.pedroPathing.Drawing;
+
 import org.firstinspires.ftc.teamcode.robot.Bob.Bob;
 
 
@@ -23,7 +28,7 @@ import org.firstinspires.ftc.teamcode.robot.Bob.Bob;
 public class TurretTele extends OpMode {
     Bob bob = new Bob();
 
-    private final double cameraHeight = 11.25; // inches
+    private final double cameraHeight = 10.76; // inches
     private final double tagHeight = 29.5; // inches
     private final double heightDif = tagHeight-cameraHeight;
     Limelight3A limelight;
@@ -40,23 +45,23 @@ public class TurretTele extends OpMode {
             throw new RuntimeException("Drawing failed " + e);
         }
     }
-
+    private Pose startPose;
     @Override
     // runs on init press
     public void init() {
-        bob.init(hardwareMap);
-        // define and init robot
-        pinpoint = hardwareMap.get(GoBildaPinpointDriver.class, "pinpoint");
-        pinpoint.resetPosAndIMU();
-        pinpoint.recalibrateIMU();
+        startPose = new Pose(LAST_X,LAST_Y,LAST_HEADING);
+
+
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(startPose);
+
+        bob.follower = follower;
+
+        bob.init(hardwareMap, false);;
 
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
         limelight.setPollRateHz(100); // This sets how often we ask Limelight for data (100 times per second)
         limelight.pipelineSwitch(0); // Switch to pipeline number 0
-
-        follower = Constants.createFollower(hardwareMap);
-        follower.setStartingPose(new Pose(86.89230769230768, 9.353846153846153, Math.toRadians(90)));
-        follower.update();
 
     }
 
@@ -65,6 +70,7 @@ public class TurretTele extends OpMode {
     public void start() {
         // run everything to start positions
         limelight.start();
+
     }
 
 
@@ -73,13 +79,14 @@ public class TurretTele extends OpMode {
     // loops after start
     // press
     public void loop() {
-        pinpoint.update();
+       // pinpoint.update();
         follower.update();
+
 
         Pose currentPose = follower.getPose();
         telemetry.addData("Pedro Pose", String.format("x=%.2f in, y=%.2f in, h=%.1f deg", currentPose.getX(), currentPose.getY(), Math.toDegrees(currentPose.getHeading())));
 
-        //Drawing.drawPoseHistory(follower.getPoseHistory());
+     //   Drawing.drawPoseHistory(follower.getPoseHistory());
         drawCurrent();
 
 
@@ -90,10 +97,10 @@ public class TurretTele extends OpMode {
             bob.motorDriveXYVectors(0.7 * -gamepad1.left_stick_x, 0.7 * gamepad1.left_stick_y, 0.3 * -gamepad1.right_stick_x);
         }
 
-        double ppYaw = pinpoint.getHeading(AngleUnit.DEGREES);
+       // double ppYaw = pinpoint.getHeading(AngleUnit.DEGREES);
 
-        telemetry.addData("ppYaw", ppYaw);
-        limelight.updateRobotOrientation(ppYaw+90);
+       // telemetry.addData("ppYaw", ppYaw);
+      //  limelight.updateRobotOrientation(follower.getHeading()+90);
 
         if (gamepad1.left_bumper){
             follower.setPose(new Pose(128.13769363166955,71.62822719449225,follower.getHeading()));
@@ -104,14 +111,19 @@ public class TurretTele extends OpMode {
         if (result != null && result.isValid()) {
             bob.turretController.update(result.getTx());
             if (Math.abs(result.getTx()) < 0.5){
-                updatePose(result.getTy());
+             //   updatePose(result.getTy());
+               // updatePose2(result.getBotposeAvgDist());
+                updatePose3(result.getBotposeAvgDist());
             }
+
+            telemetry.addLine("default distance to target: " + result.getBotposeAvgDist());
 
         } else {
             telemetry.addData("Limelight", "No Targets");
         }
 
         telemetry.update();
+
     }
     private void updatePose(double ty){
         try {
@@ -125,6 +137,56 @@ public class TurretTele extends OpMode {
             telemetry.addLine("pinpoint angle: " + Math.toDegrees(follower.getHeading()));
             telemetry.addLine("TRUE turret angle: " + trueAngle);
             if (trueAngle != 90) {
+                double visionY = Math.sin(trueAngle) * distanceFromTag;
+                double visionX = Math.cos(trueAngle) * distanceFromTag;
+                visionX = 127.628 - visionX;
+                visionY = 131.669 - visionY;
+                double finalX = currentFollowerPose.getX() + KALMAN_TURRET * (visionX - currentFollowerPose.getX());
+                double finalY = currentFollowerPose.getY() + KALMAN_TURRET * (visionY - currentFollowerPose.getY());
+                follower.setPose(new Pose(finalX, finalY, currentFollowerPose.getHeading()));
+            }
+        }
+        catch (Exception e) {
+            telemetry.addData("Limelight error", e.getMessage());
+        }
+    }
+    private void updatePose2(double dist){
+        try {
+            Pose currentFollowerPose = follower.getPose();
+            double distanceFromTag = dist*39.3701;
+            double trueAngle = Math.toDegrees(follower.getHeading()) + bob.turretController.getTurretAngle();
+            trueAngle = Math.toRadians(trueAngle);
+            telemetry.addLine("limelight distance to target: " + distanceFromTag);
+            telemetry.addLine("RELATIVE turret angle: " + bob.turretController.getTurretAngle());
+            telemetry.addLine("RELATIVE turret ticks: " + bob.turretController.getTurretTicks());
+            telemetry.addLine("pinpoint angle: " + Math.toDegrees(follower.getHeading()));
+            telemetry.addLine("TRUE turret angle: " + trueAngle);
+            if (trueAngle != 90) {
+                double visionY = Math.sin(trueAngle) * distanceFromTag;
+                double visionX = Math.cos(trueAngle) * distanceFromTag;
+                visionX = 127.628 - visionX;
+                visionY = 131.669 - visionY;
+                double finalX = currentFollowerPose.getX() + KALMAN_TURRET * (visionX - currentFollowerPose.getX());
+                double finalY = currentFollowerPose.getY() + KALMAN_TURRET * (visionY - currentFollowerPose.getY());
+                follower.setPose(new Pose(finalX, finalY, currentFollowerPose.getHeading()));
+            }
+        }
+        catch (Exception e) {
+            telemetry.addData("Limelight error", e.getMessage());
+        }
+    }
+    private void updatePose3(double dist){
+        try {
+            Pose currentFollowerPose = follower.getPose();
+            double distanceFromTag = Math.sqrt(((dist*39.3701)*(dist*39.3701))- (heightDif*heightDif));
+            double trueAngle = Math.toDegrees(follower.getHeading()) + bob.turretController.getTurretAngle();
+            trueAngle = Math.toRadians(trueAngle);
+            telemetry.addLine("limelight distance to target: " + distanceFromTag);
+            telemetry.addLine("RELATIVE turret angle: " + bob.turretController.getTurretAngle());
+            telemetry.addLine("RELATIVE turret ticks: " + bob.turretController.getTurretTicks());
+            telemetry.addLine("pinpoint angle: " + Math.toDegrees(follower.getHeading()));
+            telemetry.addLine("TRUE turret angle: " + trueAngle);
+            if (Math.toDegrees(trueAngle) != 90) {
                 double visionY = Math.sin(trueAngle) * distanceFromTag;
                 double visionX = Math.cos(trueAngle) * distanceFromTag;
                 visionX = 127.628 - visionX;
