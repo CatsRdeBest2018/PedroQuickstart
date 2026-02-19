6e67f1fcec982f967d5b3dd903e4ce8f8f76e179
darsh7378274 <adarsh.presanna@gmail.com>
2026-02-19 10:51:23 -0500

    Adjustable Hood Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/tele/AdjustableHoodServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/tele/AdjustableHoodServoTest.java
@@ -4,44 +4,50 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name = "Single Servo TeleOp", group = "Test")
+@TeleOp(name = "Servo Step Control", group = "Test")
 public class AdjustableHoodServoTest extends LinearOpMode {
 
-    Servo hood;
+    Servo myServo;
 
     double position = 0.5;
+
+
+    boolean lastA = false;
+    boolean lastB = false;
 
     @Override
     public void runOpMode() {
 
-
-        hood = hardwareMap.get(Servo.class, "hood");
-
-        hood.setPosition(position);
+        myServo = hardwareMap.get(Servo.class, "servo");
+        myServo.setPosition(position);
 
         waitForStart();
 
         while (opModeIsActive()) {
 
+            boolean currentA = gamepad1.a;
+            boolean currentB = gamepad1.b;
 
-            if (gamepad1.a) {
-                position+=0.01;
+
+            if (currentA && !lastA) {
+                position += 0.01;
             }
 
-            if (gamepad1.b) {
-                position-=0.01;
+
+            if (currentB && !lastB) {
+                position -= 0.01;
             }
-
-
-            position += -gamepad1.left_stick_y * 0.01;
 
 
             position = Math.max(0.0, Math.min(1.0, position));
 
-            hood.setPosition(position);
+            myServo.setPosition(position);
 
             telemetry.addData("Servo Position", position);
             telemetry.update();
+
+            lastA = currentA;
+            lastB = currentB;
         }
     }
 }
