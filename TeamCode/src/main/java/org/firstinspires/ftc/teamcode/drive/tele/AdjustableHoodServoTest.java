09067560b4bb8e5e6927a6ce6aa7fe9d508aa672
darsh7378274 <adarsh.presanna@gmail.com>
2026-02-27 19:31:09 -0500

    Update AdjustableHoodServoTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/tele/AdjustableHoodServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/tele/AdjustableHoodServoTest.java
@@ -5,6 +5,9 @@ import com.bylazar.telemetry.PanelsTelemetry;
 import com.bylazar.telemetry.TelemetryManager;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.CRServoImplEx;
+import com.qualcomm.robotcore.hardware.DcMotorImplEx;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.ServoImplEx;
 
@@ -13,8 +16,12 @@ import com.qualcomm.robotcore.hardware.ServoImplEx;
 public class AdjustableHoodServoTest extends LinearOpMode {
 
     ServoImplEx hood;
+    CRServoImplEx intakeRight;
+    CRServoImplEx intakeLeft;
+    DcMotorImplEx intake;
 
-    public static double position = 0.5;
+    public static double position = 0;
+    public static double power = 0;
     TelemetryManager t;
 
 
@@ -22,7 +29,16 @@ public class AdjustableHoodServoTest extends LinearOpMode {
     public void runOpMode() {
 
         hood = hardwareMap.get(ServoImplEx.class, "hood");
+        intakeRight = hardwareMap.get(CRServoImplEx.class,"intakeRight");
+        intakeLeft = hardwareMap.get(CRServoImplEx.class,"intakeLeft");
+        intake = hardwareMap.get(DcMotorImplEx.class,"intake");
+
+        intake.setDirection(DcMotorImplEx.Direction.REVERSE);
+
         hood.setPosition(position);
+        intakeRight.setPower(power);
+        intakeLeft.setPower(-power);
+        intake.setPower(power);
         t = PanelsTelemetry.INSTANCE.getTelemetry();
 
 
@@ -32,7 +48,9 @@ public class AdjustableHoodServoTest extends LinearOpMode {
             t.update();
 
             hood.setPosition(position);
-
+            intakeRight.setPower(power);
+            intakeLeft.setPower(-power);
+            intake.setPower(power);
             telemetry.addData("Servo Position", position);
             telemetry.update();
 
